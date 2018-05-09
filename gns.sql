b9dc205eeb95bbc1eb012f5634c2cec5b18f4d23
TgoislardSIO <thomas.goislard.sio@gmail.com>
2018-05-09 13:38:35 +0200

    Script BD

--- gns.sql
+++ gns.sql
@@ -34,12 +34,12 @@ CREATE TABLE Couleur(
 CREATE TABLE Partie(
         numeroPartie    Int NOT NULL ,
         dateCreation    Date ,
-		initiateur    	Int NOT NULL ,
-        adversaire  	Int NOT NULL ,
-        vainqueur  		Int NOT NULL ,
-        suivant  		Int NOT NULL ,
-        couleurInitiateur   Int NOT NULL ,
-        couleurAdversaire Int NOT NULL ,
+		initiateur    	Int  ,
+        adversaire  	Int  ,
+        vainqueur  		Int  ,
+        suivant  		Int  ,
+        couleurInitiateur   Int  ,
+        couleurAdversaire Int ,
         PRIMARY KEY (numeroPartie )
 )ENGINE=InnoDB;
 
@@ -49,3 +49,25 @@ ALTER TABLE Partie ADD CONSTRAINT FK_vainqueur FOREIGN KEY (vainqueur) REFERENCE
 ALTER TABLE Partie ADD CONSTRAINT FK_suivant FOREIGN KEY (suivant) REFERENCES Joueur(numeroJoueur);
 ALTER TABLE Partie ADD CONSTRAINT FK_couleurInitiateur FOREIGN KEY (couleurInitiateur) REFERENCES Couleur(numeroCouleur);
 ALTER TABLE Partie ADD CONSTRAINT FK_couleurAdversaire FOREIGN KEY (couleurAdversaire) REFERENCES Couleur(numeroCouleur);
+
+
+insert into Joueur values(1,"Nicolas","azerty");
+insert into Joueur values(2,"Ilona","azerty");
+insert into Joueur values(3,"Georges","azerty");
+insert into Joueur values(4,"Aicha","azerty");
+insert into Joueur values(5,"Cody","azerty");
+
+
+insert into Couleur values(1, "Blanc");
+insert into Couleur values(2, "Noir");
+
+insert into Partie values(1, '2018-05-01', 5, 2, 2, null, 1, 2);
+insert into Partie values(2, '2018-05-01', 5, 2, 5, null, 1, 2);
+insert into Partie values(3, '2018-05-01', 5, null, null, 5, 1, 2);
+insert into Partie values(4, '2018-05-01', 2, null, null, null, 2, null);
+insert into Partie values(5, '2018-05-02', 5, 1, null, 1, 1, 2);
+insert into Partie values(6, '2018-05-02', 5, 1, null, 5, 1,2);
+insert into Partie values(7, '2018-05-02', 1, null, null, null, null, 2);
+insert into Partie values(8, '2018-05-02', 1, null, null, null, null, 2);
+insert into Partie values(9, '2018-05-03', 5, 2, null, 2, 1, 2);
+insert into Partie values(10, '2018-05-03', 2, 1, 2, null, 2, 1);
